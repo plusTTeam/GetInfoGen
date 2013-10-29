dc9cd6c26d83f31fb15a9f55a9108b841fb0a739
jesus.lopezlugo <jesus.lopezlugo@8bec8e58-b661-5a17-0618-20272cf55a6b>
2013-10-29 01:06:27 +0000

    First Commit, Welcome Team :)

---
+++ GetInfoGen/src/InfoGen/Gen.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InfoGen;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Gen {
+    private int[] gen;
+    private int[] atg;
+    private int[] gt;
+    private int[] ag;
+    private int[] paradas;
+    private int[] intrones;
+    private String[] intrones_data;
+    private int[] exones;
+    private String[] exones_data;
+
+    public int[] getGen() {
+        return gen;
+    }
+
+    public void setGen(int[] gen) {
+        this.gen = gen;
+    }
+
+    public int[] getAtg() {
+        return atg;
+    }
+
+    public void setAtg(int[] atg) {
+        this.atg = atg;
+    }
+
+    public int[] getGt() {
+        return gt;
+    }
+
+    public void setGt(int[] gt) {
+        this.gt = gt;
+    }
+
+    public int[] getAg() {
+        return ag;
+    }
+
+    public void setAg(int[] ag) {
+        this.ag = ag;
+    }
+
+    public int[] getParadas() {
+        return paradas;
+    }
+
+    public void setParadas(int[] paradas) {
+        this.paradas = paradas;
+    }
+
+    public int[] getIntrones() {
+        return intrones;
+    }
+
+    public void setIntrones(int[] intrones) {
+        this.intrones = intrones;
+    }
+
+    public String[] getIntrones_data() {
+        return intrones_data;
+    }
+
+    public void setIntrones_data(String[] intrones_data) {
+        this.intrones_data = intrones_data;
+    }
+
+    public int[] getExones() {
+        return exones;
+    }
+
+    public void setExones(int[] exones) {
+        this.exones = exones;
+    }
+
+    public String[] getExones_data() {
+        return exones_data;
+    }
+
+    public void setExones_data(String[] exones_data) {
+        this.exones_data = exones_data;
+    }
+    
+    
+}
---
+++ GetInfoGen/src/InfoGen/GetInfoGen.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InfoGen;
+
+/**
+ *
+ * @author Jesus
+ */
+public class GetInfoGen {
+    
+    
+    public void JPL(String commandProlog){
+    
+    }
+}
