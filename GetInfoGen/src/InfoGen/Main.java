a8c16c26c51f3577a0d784593074c1ecb15611ab
jesus.lopezlugo <jesus.lopezlugo@8bec8e58-b661-5a17-0618-20272cf55a6b>
2013-11-03 22:51:28 +0000

    Cambios Menores

--- GetInfoGen/src/InfoGen/Main.java
+++ GetInfoGen/src/InfoGen/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String [ ] args){
         gig = new Gen();
        middleWare middle = new middleWare();
-        middle.init("C:/gen_prueba.pl");
+        middle.init("C:/p_genes.pl");
         middle.consultEverything();        
         gig.setAg(middle.getAgPositions());
         gig.setAtg(middle.getAtgPositions());
