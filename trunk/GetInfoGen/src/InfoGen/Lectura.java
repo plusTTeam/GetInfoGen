c186ae52b0be5223bfb47759fab1748dc3dba075
jesus.lopezlugo <jesus.lopezlugo@8bec8e58-b661-5a17-0618-20272cf55a6b>
2013-11-03 04:16:20 +0000

    Get Gen Information Beta 1.0

--- trunk/GetInfoGen/src/InfoGen/Lectura.java
+++ trunk/GetInfoGen/src/InfoGen/Lectura.java
@@ -12,6 +12,7 @@ import java.util.List;
  * @author Jesus
  */
 public class Lectura {
+
     private List<String> gen;
     private Integer atg;
     private List<Par> intrones;
@@ -19,7 +20,7 @@ public class Lectura {
     private Integer parada;
     private List<String> intrones_data;
     private List<String> exones_data;
-    
+
     //Formato Intron [2,(5,8),(12,15),20]
     //Formato Intron Data [atg,(gtafasag),(gtaasdag),taa]
     //Formato Exon [(2,4),(9,11),(16,20)]
@@ -27,13 +28,15 @@ public class Lectura {
     public Lectura() {
     }
 
-    public Lectura(List<String> gen,Integer atg, List<Par> intrones, Integer parada) {
+    public Lectura(List<String> gen, Integer atg, List<Par> intrones, Integer parada) {
         this.atg = atg;
         this.intrones = intrones;
         this.parada = parada;
         this.gen = gen;
         this.intrones_data = new ArrayList<>();
         this.exones_data = new ArrayList<>();
+        this.exones = new ArrayList<>();
+        calcularExones();
         cargarData();
     }
 
@@ -84,20 +87,68 @@ public class Lectura {
     public void setGen(List<String> gen) {
         this.gen = gen;
     }
-    private void cargarData(){
-        if(!gen.isEmpty() && !intrones.isEmpty()){
-            for (Par intronesaux : intrones) {
-               String aux = "(";
+    private void calcularExones(){
+        if(intrones!=null){
+            exones.add(new Par(atg, intrones.get(0).getVal1()-1));
+            for (int i = 0; i < intrones.size()-1; i++) {
+                exones.add(new Par(intrones.get(i).getVal2()+1, intrones.get(i+1).getVal1()-1));
+            }
+            exones.add(new Par(intrones.get(intrones.size()-1).getVal2()+1, parada));
+        }else{
+            exones.add(new Par(atg,parada));
+        }
+    }
+    private void cargarData() {
+        if (!gen.isEmpty() && !intrones.isEmpty() && !exones.isEmpty()) {            
+            for (Par intronesaux : intrones) {   
+                String aux="";
                 for (int i = intronesaux.getVal1(); i <= intronesaux.getVal2(); i++) {
-                    aux+=""+gen.get(i)+",";
+                    aux+=""+gen.get(i);
                 }
-                aux = aux.substring(0, aux.length()-1);
-                aux += ")";
-                intrones_data.add(aux);
+                intrones_data.add(aux) ;                
+            }
+             for (Par exonesaux : exones) {   
+                String aux="";
+                for (int i = exonesaux.getVal1(); i <= exonesaux.getVal2(); i++) {
+                    aux+=""+gen.get(i);
+                }
+                exones_data.add(aux) ;                
             }
         }
     }
-    public String ImprimirLectura(){
-        return gen.get(atg)+intrones_data+gen.get(parada);
+
+    public String ImprimirLecturaIntron() {
+        String cadena = "["+atg+",";
+        for (Par intronaux : intrones) {
+            cadena += "("+intronaux.getVal1()+","+intronaux.getVal2()+"),";
+        }
+        cadena += parada+"]";
+        return cadena;
+    }
+     public String ImprimirLecturaIntronData() {
+        String cadena = "["+gen.get(atg)+",";
+        for (String intronaux : intrones_data) {
+            cadena += "("+intronaux+"),";
+        }
+        cadena += gen.get(parada)+"]";
+        return cadena;
+    }
+     public String ImprimirLecturaExon() {
+        String cadena = "[";
+        for (Par exonaux : exones) {
+            cadena += "("+exonaux.getVal1()+","+exonaux.getVal2()+"),";
+        }
+        cadena = cadena.substring(0,cadena.length()-1);
+        cadena+="]";
+        return cadena;
+    }
+     public String ImprimirLecturaExonData() {
+        String cadena = "[";
+        for (String exonaux : exones_data) {
+            cadena += "("+exonaux+"),";
+        }
+        cadena = cadena.substring(0,cadena.length()-1);
+        cadena+="]";
+        return cadena;
     }
 }
